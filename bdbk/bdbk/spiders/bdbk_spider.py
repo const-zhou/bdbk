09b4513c28973619d50fa8e9abc512d1e54e9172
zhoudunguang <zhoudunguang@sogou-inc.com>
2017-03-21 19:27:54 +0800

    add bdbk info

--- bdbk/bdbk/spiders/bdbk_spider.py
+++ bdbk/bdbk/spiders/bdbk_spider.py
@@ -4,13 +4,17 @@ import scrapy
 
 class BDBKSpider(scrapy.Spider):
     name = "bdbk"
-    allowed_domains = ["dmoz.org"]
+    # allowed_domains = ["dmoz.org"]
     start_urls = [
-        "http://www.dmoz.org/Computers/Programming/Languages/Python/Books/",
-        "http://www.dmoz.org/Computers/Programming/Languages/Python/Resources/"
+        "http://baike.baidu.com/item/苹果/5670"
+        # "http://www.dmoz.org/Computers/Programming/Languages/Python/Resources/"
     ]
 
     def parse(self, response):
-        filename = response.url.split("/")[-2] + '.html'
-        with open(filename, 'wb') as f:
-            f.write(response.body)
+        for sel in response.xpath("//div[@class='lemma-summary']/div[@class='para']"):
+            title = sel.xpath('a/text()').extract()
+            # link = sel.xpath('a/@href').extract()
+            desc = sel.xpath('text()').extract()
+            for info in desc:
+                print info
+            
